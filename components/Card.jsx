133b2f84608461f8659db8b5eb58172ff014886c
supriya-kotturu <supriyakotturu@pm.me>
2021-06-06 23:08:44 +0530

    fix : styling errors

--- components/Card.jsx
+++ components/Card.jsx
@@ -15,55 +15,60 @@ const Card = (props) => {
   console.log("here me : ", props);
 
   return (
-    <View style={tailwind("m-4 w-64 rounded-xl relative")}>
-      <LinearGradient
-        start={{ x: 0.0, y: 0.35 }}
-        end={{ x: 0.2, y: 0.8 }}
-        locations={[0, 0.2, 0.3, 0.8, 0.9]}
-        colors={[
-          "#9d4eddff",
-          "#5a189aff",
-          "#3c096cff",
-          "#240046ff",
-          "#240046ff",
-        ]}
-        style={tailwind("rounded-xl p-2")}
-      >
-        <View style={styles.card}>
-          {/* <Image style={} source = {{uri : ''}}/>  */}
-          <Text style={styles.movieText}>StarWars : Ep 1</Text>
-          <View style={tailwind("my-8")}>
-            <Text style={styles.date}>Release Date 12-Apr-2010</Text>
-            <Text style={styles.director}>Director</Text>
+    <View style={styles.container}>
+      <View style={tailwind("w-64 rounded-xl relative")}>
+        <LinearGradient
+          start={{ x: 0.0, y: 0.35 }}
+          end={{ x: 0.2, y: 0.8 }}
+          locations={[0, 0.2, 0.3, 0.8, 0.9]}
+          colors={[
+            "#9d4eddff",
+            "#5a189aff",
+            "#3c096cff",
+            "#240046ff",
+            "#240046ff",
+          ]}
+          style={tailwind("rounded-xl p-2")}
+        >
+          <View style={styles.card}>
+            {/* <Image style={} source = {{uri : ''}}/>  */}
+            <Text style={styles.movieText}>StarWars : Ep 1</Text>
+            <View style={tailwind("my-8")}>
+              <Text style={styles.date}>Release Date 12-Apr-2010</Text>
+              <Text style={styles.director}>Director</Text>
+            </View>
+            <TouchableOpacity>
+              <Button
+                title="Know More"
+                color="#ff8500ff"
+                style={styles.button}
+                onPress={() =>
+                  props.navigation.navigate({ routeName: "Characters" })
+                }
+              />
+            </TouchableOpacity>
           </View>
-          <TouchableOpacity>
-            <Button
-              title="Know More"
-              color="#ff8500ff"
-              style={styles.button}
-              onPress={() =>
-                props.navigation.navigate({ routeName: "Characters" })
-              }
-            />
-          </TouchableOpacity>
-        </View>
-      </LinearGradient>
+        </LinearGradient>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  card: {
-    padding: 16,
-    paddingBottom: 32,
-    alignItems: "center",
-    justifyContent: "center",
+  container: {
+    margin: 16,
     shadowColor: "rgba(90, 24, 154, 1)",
     borderRadius: 12,
     shadowOffset: { width: 2, height: 10 },
     shadowOpacity: 0.35,
     shadowRadius: 18,
     elevation: 8,
+  },
+  card: {
+    padding: 16,
+    paddingBottom: 32,
+    alignItems: "center",
+    justifyContent: "center",
   },
   movieText: {
     color: "rgba(243, 244, 246,1)",
--- components/Characters.jsx
+++ components/Characters.jsx
@@ -8,23 +8,31 @@ const Characters = () => {
     <View style={styles.characterCard}>
       <View
         style={tailwind(
-          "bg-white rounded-2xl md:w-1/2 m-4 p-2 m-1 flex flex-row flex-wrap justify-center items-center"
+          "bg-white rounded-2xl sm:w-full md:w-1/2 m-4 p-2 m-1 flex flex-wrap flex-row justify-center items-center"
         )}
       >
         <View>
           <Image
             source={require("../assets/img/64.jpg")}
-            style={tailwind("h-32 w-32 rounded-full mx-auto m-2")}
+            style={tailwind(
+              "h-24 w-24 md:h-32 md:w-32 rounded-full mx-auto m-2"
+            )}
           />
-        </View>
-        <View>
           <View style={tailwind("p-1 pl-2")}>
-            <Text style={tailwind("font-bold text-lg text-purple text-left")}>
+            <Text style={tailwind("font-bold text-lg text-purple text-center")}>
               Princess Leia
             </Text>
-            <Text style={tailwind("text-purple text-left")}>Carrie Fisher</Text>
+            <Text style={tailwind("text-purple text-center")}>
+              Carrie Fisher
+            </Text>
           </View>
-          <Text style={tailwind("w-64 p-2")}>
+        </View>
+        <View>
+          <Text
+            style={tailwind(
+              "h-24 w-52 break-words overflow-hidden px-2 pb-2 mb-4"
+            )}
+          >
             Instead of being dismissed as another “damsel in distress,” Leia is
             a fighter, more than capable of taking care of herself, which she
             amply demonstrates by strangling Jabba the Hutt to death in Return
