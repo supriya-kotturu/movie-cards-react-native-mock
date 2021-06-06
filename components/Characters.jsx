a91fd45ef81a849840b1043e062979bde40ac197
supriya-kotturu <supriyakotturu@pm.me>
2021-06-06 21:59:20 +0530

    remove : unused imports in files

--- components/Characters.jsx
+++ components/Characters.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Image, StyleSheet, ImageBackground } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 
 import { tailwind } from "../tailwind";
 
--- containers/CardContainer.jsx
+++ containers/CardContainer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, ScrollView, View } from "react-native";
+import { ScrollView, View } from "react-native";
 
 import { tailwind } from "../tailwind";
 
